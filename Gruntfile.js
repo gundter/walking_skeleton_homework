2b0dbc973338cb06d768c5b0fe94cdd2d1f60fbc
Terry Gunderson <tgunderson25@gmail.com>
2015-05-05 17:50:57 -0500

    first commit

---
+++ Gruntfile.js
@@ -0,0 +1,45 @@
+module.exports = function(grunt){
+    //Project configuration.
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        uglify: {
+            options: {
+                banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+            },
+            build: {
+                src: 'client/app.js',
+                dest: 'server/public/assets/scripts/app.min.js'
+            }
+        },
+        copy: {
+            main: {
+                expand: true,
+                cwd: "node_modules/",
+                src: [
+                    "angular/angular.min.js",
+                    "angular/angular.min.js.map",
+                    "angular/angular-csp.css",
+                    "bootstrap/dist/js/bootstrap.min.js",
+                    "bootstrap/dist/css/bootstrap.min.css",
+                    "bootstrap/dist/css/bootstrap.css.map",
+                    "jquery/dist/jquery.min.js",
+                    "jquery/dist/jquery.min.map"
+                ],
+                "dest": "server/public/vendor/"
+            },
+            css: {
+                expand: true,
+                cwd: "client/",
+                src: "stylesheet.css",
+                "dest": "server/public/assets/css/"
+            }
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-copy');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+
+    //Default task(s)
+    grunt.registerTask('default', ['copy', 'uglify']);
+
+};
